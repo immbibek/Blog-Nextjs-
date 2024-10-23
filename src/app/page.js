4aa8c2525106a93eacd52f6f9c473494bd0914ea
Bibek <137331749+immbibek@users.noreply.github.com>
2024-10-24 01:20:46 +0530

    working on comp. card and change in homepage.css

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+// import styles from "./page.module.css";
 import Link from "next/link";
 import Featured from "@/components/featured/Featured";
 import CategoryList from "@/components/categoryList/CategoryList";
 import CardList from "@/components/cardList/CardList";
 import Menu from "@/components/Menu/Menu";
-
+import styles from "./homepage.module.css"
 
 export default function Home() {
   return (
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from "./menu.module.css"
 const Menu = () => {
   return (
-    <div className={styles.container}>Navbar</div>
+    <div className={styles.container}>Menu</div>
   )
 }
 
